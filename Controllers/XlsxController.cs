a019aef21ec335f8c89148887e3f5747d88499ad
Arthur Pereira <arthuraapsilva@gmail.com>
2021-11-07 20:49:30 -0300

    PRIMEIRO COMMIT

---
+++ Controllers/XlsxController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Read.XLSX.WEBAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class XlsxController : ControllerBase
+    {
+        public async Task<ActionResult> GetValores()
+        {
+            
+            
+            return Ok();
+        }
+    }
+}
---
+++ Read.XLSX.Application/Applications/GetValoresApplication.cs
@@ -0,0 +1,16 @@
+﻿using Read.XLSX.Domain.Contracts.Application;
+using System;
+using System.Threading.Tasks;
+
+namespace Read.XLSX.Application.Applications
+{
+    public class GetValoresApplication : IGetValoresApplication
+    {
+        public Task GetValoresXlsx()
+        {
+            var fileName = @"C:\Temp\Names.xlsx"; // your Excel file
+            var people = new ExcelMapper(fileName).Fetch<Person>();
+            throw new NotImplementedException();
+        }
+    }
+}
